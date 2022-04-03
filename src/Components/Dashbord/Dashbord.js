bcf006da93c3df43efd49b247338c61e133fc35c
elaheerazob <ridwanelahee282@gmail.com>
2022-04-04 01:17:39 +0600

    chart add

--- src/Components/Dashbord/Dashbord.js
+++ src/Components/Dashbord/Dashbord.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashbord = () => {
+    const [chartData,setChartData] =useState([])
+    useEffect( () =>{
+        fetch('https://raw.githubusercontent.com/ProgrammingHero1/product-analysis-website/main/data.json')
+        .then(res => res.json())
+        .then(data => setChartData(data))
+    },[])
     return (
         <div>
-            <h1>This is Dashbord</h1>
+            <LineChart width={730} height={250} data={chartData}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                <Line type="monotone" dataKey="investment" stroke="#8884d8" />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
         </div>
     );
 };
